3ca47a7705d9978dbedde55b4a42166081e4f211
Samuel Santos <samuelsts.dev@gmail.com>
2020-09-03 17:11:18 -0300

    Change from gradle to maven, because protobuf-gen plugin conflicts with quarkus-grpc generate duplicate files

--- product-list/src/main/java/com/samuelssantos/ProductResource.java
+++ product-list/src/main/java/com/samuelssantos/ProductResource.java
@@ -2,17 +2,28 @@ package com.samuelssantos;
 
 import com.samuelssantos.dto.Discount;
 import com.samuelssantos.dto.Product;
+import io.quarkus.grpc.runtime.annotations.GrpcService;
+import org.jboss.logging.Logger;
 
+import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Set;
+import com.samuelssantos.pb.DiscountCalculatorServiceGrpc.DiscountCalculatorServiceBlockingStub;
+import com.samuelssantos.pb.DiscountRequest;
 
 @Path("/product")
 @Produces(MediaType.APPLICATION_JSON)
 public class ProductResource {
+
+    private static final Logger logger = Logger.getLogger(ProductResource.class);
+
+    @Inject
+    @GrpcService("discount-calculator")
+    DiscountCalculatorServiceBlockingStub serviceBlockingStub;
 
     private Set<Product> products = Collections.newSetFromMap(Collections.synchronizedMap(new LinkedHashMap<>()));
 
@@ -42,5 +53,13 @@ public class ProductResource {
                 .findAny()
                 .orElseThrow(NotFoundException::new);
         return Response.ok(product).build();
+    }
+
+    @GET
+    @Path("/rpc/{id}")
+    public void rpc(@PathParam("id") String id) {
+        String msg = serviceBlockingStub.process(
+                DiscountRequest.newBuilder().setProductId("9090").setUserId(id).build()).getMsg();
+        logger.log(Logger.Level.INFO, msg);
     }
 }
--- product-list/src/native-test/java/com/samuelssantos/NativeProductResourceIT.java
+++
@@ -1,9 +0,0 @@
-package com.samuelssantos;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeProductResourceIT extends ProductResourceTest {
-
-    // Execute the same tests but in native mode.
-}
