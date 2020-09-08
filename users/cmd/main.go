f74680dd60d6aeb8487c08e4f2c47685b48e97c4
Samuel Santos <samuelsts.dev@gmail.com>
2020-09-08 16:44:29 -0300

    Add config for database

---
+++ users/cmd/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	users "github.com/SamuelsSantos/product-discount-service/users/pb"
+
+	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	router := gin.Default()
+
+	rg := router.Group("api/v1/user")
+	{
+		rg.GET("/:id", fetchUser)
+	}
+
+	router.Run()
+}
+
+func fetchUser(c *gin.Context) {
+	sAddress := "localhost:8485"
+	conn, e := grpc.Dial(sAddress, grpc.WithInsecure())
+	if e != nil {
+		log.Fatalf("Failed to connect to server %v", e)
+	}
+	defer conn.Close()
+
+	client := users.NewUserServiceClient(conn)
+	player, e := client.GetByID(context.Background(), &users.Request{
+		Id: c.Param("id"),
+	})
+	if e != nil {
+		log.Fatalf("Failed to get player data: %v", e)
+	}
+
+	c.JSON(200, &player)
+}
