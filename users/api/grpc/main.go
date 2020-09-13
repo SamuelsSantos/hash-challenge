4fdc11b9d4419f22be1a4f907d501c87f84bded0
Samuel Santos <samuelsts.dev@gmail.com>
2020-09-13 18:48:53 -0300

    Fix default server port

--- users/api/grpc/main.go
+++ users/api/grpc/main.go
@@ -20,7 +20,7 @@ func main() {
 	repository := domain.NewSQLRepository(cfg)
 	service := domain.NewUserService(repository)
 
-	port := flag.Int("port", 8485, "the server port")
+	port := flag.Int("port", 50001, "the server port")
 	flag.Parse()
 	log.Printf("start server on port %d", *port)
 
